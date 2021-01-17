791c6eefaabb3c56c6cf4076ea09f42e19cfc0a4
Peter Brennan <peter.brennan@wsu.edu>
2021-01-16 17:56:09 -0800

    We are now drawing

--- src/main.js
+++ src/main.js
@@ -3,22 +3,35 @@ const fs = require('fs');
 const { createFFmpeg, fetchFile } = require('ffmpeg');
 const fabric = require("fabric").fabric;
 
-// test that browserify is working
-var unique = require('uniq');
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
-console.log(unique(data));
-
 // create a canvas element in the dom
+//let d = document.createElement('div')
+//d.style.cssText = "width: 100vw; height: 100vh; color: blue; border: 1px solid black; margin: 0; padding: 0;";
 let d = document.createElement('div')
-d.style.cssText = "width: 100vw; height: 100vh; color: blue; border: 1px solid black; margin: 0; padding: 0;";
+d.id = "wrapper";
+d.style = "border: 1px solid; max-width: fit-content; height: 100%;"
 document.body.appendChild(d);
+
 let c = document.createElement('canvas')
-c.style.cssText = "width: 500px; height: 500px; border: 1px solid black";
+c.id = "c";
 d.appendChild(c);
 
 // link to the canvas with fabric 
 var canvas = new fabric.Canvas('c');
-var rect = new fabric.Rect();
+canvas.setHeight(500);
+canvas.setWidth(500);
+
+
+var rect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: 'red',
+  width: 20,
+  height: 20,
+  angle: 45
+});
 
 canvas.add(rect);
+
+canvas.isDrawingMode = true;
+
 
