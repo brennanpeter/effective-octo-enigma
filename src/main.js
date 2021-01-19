735f0e06c17cfec50ec026c3d799fcf96497a801
Peter Brennan <peter.brennan@wsu.edu>
2021-01-18 22:13:40 -0800

    more stuff idk man lmao

--- src/main.js
+++ src/main.js
@@ -1,37 +1,91 @@
 // import all dependencies from node_modules
 const fs = require('fs');
-const { createFFmpeg, fetchFile } = require('ffmpeg');
+const ffmpeg = require('ffmpeg');
 const fabric = require("fabric").fabric;
+const fileDialog = require('file-dialog');
 
-// create a canvas element in the dom
-//let d = document.createElement('div')
-//d.style.cssText = "width: 100vw; height: 100vh; color: blue; border: 1px solid black; margin: 0; padding: 0;";
-let d = document.createElement('div')
-d.id = "wrapper";
-d.style = "border: 1px solid; max-width: fit-content; height: 100%;"
-document.body.appendChild(d);
+var canvas;
 
-let c = document.createElement('canvas')
-c.id = "c";
-d.appendChild(c);
+// ********** DOWNLOAD ************
+// yoinked from https://stackoverflow.com/questions/33806624/save-fabricjs-canvas-as-image-on-the-pc
+function downloadFiles(e) {
+  canvas.toDataURL({
+    format: 'png',
+    quality: 0.8
+  });
 
-// link to the canvas with fabric 
-var canvas = new fabric.Canvas('c');
-canvas.setHeight(500);
-canvas.setWidth(500);
+}
 
 
-var rect = new fabric.Rect({
-  left: 100,
-  top: 100,
-  fill: 'red',
-  width: 20,
-  height: 20,
-  angle: 45
-});
+// ********* UPLOAD *************
 
-canvas.add(rect);
+//load an mp4 file using the dialog
 
-canvas.isDrawingMode = true;
+function makeLayout(){
+  // create a canvas element in the dom
+  //let d = document.createElement('div')
+  //d.style.cssText = "width: 100vw; height: 100vh; color: blue; border: 1px solid black; margin: 0; padding: 0;";
+  let cols = document.createElement('div');
+  cols.style = "flex-direction: row;"
+  document.body.appendChild(cols);
+
+  let col1 = document.createElement('div');
+  col1.id = "canvas-wrapper";
+  col1.style = "border: 1px solid; max-width: fit-content; height: 100%;"
+  cols.appendChild(col1);
+
+  let c = document.createElement('canvas')
+  c.id = "c";
+  col1.appendChild(c);
+
+  // link to the canvas with fabric 
+  canvas = new fabric.Canvas('c');
+  canvas.setHeight(500);
+  canvas.setWidth(500);
+
+  let upload = document.createElement('button');
+  upload.textContent = "Upload a video";
+  upload.onclick = processFiles();
+  
+  col1.appendChild(upload);
+
+  let expor = document.createElement('button');
+  expor.textContent = "Export a video";
+  expor.addEventListener('click', downloadFiles, false);
+  col1.appendChild(expor);
+
+  // create a second column for the toolbar
+  let col2 = document.createElement('div');
+  col2.id = "canvas-wrapper";
+  col2.style = "border: 1px solid; max-width: fit-content; height: 100%;"
+  cols.appendChild(col2);
+
+}
+function addSquareToCanvas(){
+  // test drawing a rectangle on the canvas
+  var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20,
+    angle: 45
+  });
+  canvas.add(rect);
+
+}
+
+function toggleDrawingMode(){
+  // set the canvas to be in drawing mode
+  canvas.isDrawingMode = !(canvas.isDrawingMode)
+}
 
 
+function main(){
+  makeLayout();
+  addSquareToCanvas();
+  toggleDrawingMode();
+
+}
+
+main();
